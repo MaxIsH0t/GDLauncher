741a8daaeb85ba8a2bc53384954dcd666c2a015d
Gianmarco <cavallogianmarco@gmail.com>
2019-06-11 10:46:37 +0200

    autoapdate removed

--- app/main.dev.js
+++ app/main.dev.js
@@ -184,41 +184,6 @@ if (minimist(process.argv.slice(1)).i) {
     });
     let checked = false;
 
-    ipcMain.on('check-for-updates', ev => {
-      // Avoid doing this more than 1 time. It breaks everything
-      if (checked === true) return;
-      autoUpdater.checkForUpdates();
-      checked = true;
-      log.info('CHECK_FOR_UPDATES');
-
-      autoUpdater.on('update-available', info => {
-        log.info('DOWNLOAD_AVAILABLE');
-        ev.sender.send('update-available');
-      });
-
-      autoUpdater.on('update-downloaded', info => {
-        log.info('UPDATE_DOWNLOADED');
-        ev.sender.send('update-downloaded');
-      });
-
-      autoUpdater.on('download-progress', data => {
-        log.info(data);
-        ev.sender.send('download-progress', Math.floor(data.percent));
-      });
-    });
-
-    ipcMain.on('download-updates', () => {
-      log.info('DOWNLOAD_UPDATES');
-      autoUpdater.downloadUpdate();
-    });
-
-    ipcMain.on('apply-updates', () => {
-      log.info('APPLY_UPDATES');
-      autoUpdater.quitAndInstall(true, true);
-      // app.relaunch({ args: process.argv.slice(1).concat(['--relaunch']) });
-      // app.exit();
-    });
-
     ipcMain.on('open-devTools', () => {
       mainWindow.webContents.openDevTools({ mode: 'undocked' });
     });
